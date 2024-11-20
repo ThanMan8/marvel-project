c4ee2020cf2d42e98eb1c060bf068c11547a74d7
Thanos <manoloudas88@gmail.com>
2024-11-20 22:40:19 +0200

    add ig

--- creating-airflow/variables.tf
+++ creating-airflow/variables.tf
@@ -39,3 +39,9 @@ variable "private_subnets" {
     private-2 = "10.192.21.0/24"
   }
 }
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24" # Example CIDR block; adjust as needed
+}
