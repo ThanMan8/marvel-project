d79abd1fc7537ad38b6f2d8143bf48f180b343f2
Thanos <manoloudas88@gmail.com>
2024-11-20 19:29:54 +0200

    change the availability zone

--- creating-airflow/resources.tf
+++ creating-airflow/resources.tf
@@ -87,8 +87,7 @@ resource "aws_subnet" "private" {
   for_each                = var.private_subnets
   vpc_id                  = aws_vpc.vpc-airflow.id
   cidr_block              = each.value
-  availability_zone       = element(data.aws_availability_zones.available.names, index(var.private_subnets, each.key)) # Ensure different AZ for each subnet
-  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, index(keys(var.private_subnets), each.key))
 
   tags = {
     Name = "${each.key}-subnet"
